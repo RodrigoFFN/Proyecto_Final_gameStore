c819d53cbd87a38830f1c7a79becd283d27459a6
Fabricio Jimnez <fjimenez@unsa.edu.pe>
2025-07-05 22:35:38 -0500

    Add API ViewSets for all models using Django REST Framework

--- gameStore/backend/ESGames/views.py
+++ gameStore/backend/ESGames/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models.cart import Cart, CartItem
+from .models.category import Category
+from .models.library import Library
+from .models.rating import Rating
+from .models.review import Review
+from .models.user_profile import UserProfile
+from .models.videogame import Videogame
+from .serializers import (
+    CartSerializer, CartItemSerializer,
+    CategorySerializer, LibrarySerializer,
+    RatingSerializer, ReviewSerializer,
+    UserProfileSerializer, VideogameSerializer
+)
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class LibraryViewSet(viewsets.ModelViewSet):
+    queryset = Library.objects.all()
+    serializer_class = LibrarySerializer
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class VideogameViewSet(viewsets.ModelViewSet):
+    queryset = Videogame.objects.all()
+    serializer_class = VideogameSerializer
