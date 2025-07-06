bb9c1161ae419f4b16c395b015fbea5a8a494d06
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-05 22:52:32 -0500

    Register models from app to admin panel

--- gameStore/backend/ESGames/admin.py
+++ gameStore/backend/ESGames/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    UserProfile,
+    Videogame,
+    Category,
+    Cart,
+    CartItem,
+    Review,
+    Rating,
+    Library,
+)
+
+admin.site.register(UserProfile)
+admin.site.register(Videogame)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Review)
+admin.site.register(Rating)
+admin.site.register(Library)
