84a910cfeaef6651c14f5ba64ecffe442ad9b5a1
Fabricio Jimnez <fjimenez@unsa.edu.pe>
2025-07-05 22:38:11 -0500

    Set up API routing using DefaultRouter and registered all ViewSets

---
+++ gameStore/backend/ESGames/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CartViewSet, CartItemViewSet,
+    CategoryViewSet, LibraryViewSet,
+    RatingViewSet, ReviewViewSet,
+    UserProfileViewSet, VideogameViewSet
+)
+
+router = DefaultRouter()
+router.register(r'cart', CartViewSet)
+router.register(r'cartitem', CartItemViewSet)
+router.register(r'category', CategoryViewSet)
+router.register(r'library', LibraryViewSet)
+router.register(r'rating', RatingViewSet)
+router.register(r'review', ReviewViewSet)
+router.register(r'userprofile', UserProfileViewSet)
+router.register(r'videogame', VideogameViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
