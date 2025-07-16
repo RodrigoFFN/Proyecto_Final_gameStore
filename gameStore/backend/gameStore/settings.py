78e721dc33fb316d1ae3ab3bc3e35442254f77e3
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-16 09:39:20 -0500

    Enable SessionAuthentication to allow admin UI testing.

--- gameStore/backend/gameStore/settings.py
+++ gameStore/backend/gameStore/settings.py
@@ -44,9 +44,15 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication', 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
