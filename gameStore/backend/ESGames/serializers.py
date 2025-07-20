c846870e043c25349e80c68db8c9a5e948b42b30
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-20 18:45:02 -0500

    Add register serializer with create function.

--- gameStore/backend/ESGames/serializers.py
+++ gameStore/backend/ESGames/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models.cart import Cart, CartItem
 from .models.category import Category
 from .models.library import Library
@@ -46,3 +47,24 @@ class VideogameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Videogame
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    address = serializers.CharField(write_only=True, required=False)
+    phone = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'address', 'phone']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        address = validated_data.pop('address', '')
+        phone = validated_data.pop('phone', '')
+
+        user = User.objects.create_user(**validated_data)
+
+        UserProfile.objects.create(user=user, address=address, phone=phone)
+
+        return user
