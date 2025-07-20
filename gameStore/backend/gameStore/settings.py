9583345492d83d383e1761fe297c33ea600ea1e9
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-20 15:16:09 -0500

    Configure CORS to allow requests.

--- gameStore/backend/gameStore/settings.py
+++ gameStore/backend/gameStore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ESGames',
     'rest_framework',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -60,6 +61,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +70,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]
+
 
 ROOT_URLCONF = 'gameStore.urls'
 
