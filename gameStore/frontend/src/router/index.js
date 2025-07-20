6c3626c65f546cf1ec3ee70f9e32091431a70c8a
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-20 18:33:44 -0500

    Add register path to index.js from router.

--- gameStore/frontend/src/router/index.js
+++ gameStore/frontend/src/router/index.js
@@ -2,29 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
- 
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ('../views/Home.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
