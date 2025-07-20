dbfc45e6472d412b6fd6df43dc8d7c7ecacad459
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-20 18:46:06 -0500

    Add RegisterView with allowany permissions.

--- gameStore/backend/ESGames/views.py
+++ gameStore/backend/ESGames/views.py
@@ -1,22 +1,17 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from .models.cart import Cart, CartItem
-from rest_framework.response import Response
-from rest_framework import status
 from .models.category import Category
 from .models.library import Library
 from .models.rating import Rating
 from .models.review import Review
 from .models.user_profile import UserProfile
 from .models.videogame import Videogame
-from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import (
     CartSerializer, CartItemSerializer,
     CategorySerializer, LibrarySerializer,
     RatingSerializer, ReviewSerializer,
-    UserProfileSerializer, VideogameSerializer
+    UserProfileSerializer, VideogameSerializer, RegisterSerializer
 )
 
 class CartViewSet(viewsets.ModelViewSet):
@@ -59,28 +54,7 @@ class VideogameViewSet(viewsets.ModelViewSet):
     serializer_class = VideogameSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-@api_view(['POST'])
-def register_user(request):
-    username = request.data.get('username')
-    email = request.data.get('email')
-    password = request.data.get('password')
-    address = request.data.get('address', '')
-    phone = request.data.get('phone', '')
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
-    if not username or not password:
-        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    if User.objects.filter(username=username).exists():
-        return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    user = User.objects.create_user(username=username, password=password, email=email)
-
-    UserProfile.objects.create(user=user, address=address, phone=phone)
-
-    refresh = RefreshToken.for_user(user)
-
-    return Response({
-        'message': 'User and profile created successfully.',
-        'access': str(refresh.access_token),
-        'refresh': str(refresh)
-    }, status=status.HTTP_201_CREATED)
