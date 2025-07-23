e6b9fd881a88a44e16519a12d13969e3f611a8d0
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 18:20:39 -0500

    Add BootstrapVue 3 integration with styles and plugin setup.

--- gameStore/frontend/src/main.js
+++ gameStore/frontend/src/main.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+app.use(BootstrapVue3)
 app.mount('#app')
