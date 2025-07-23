1cd8540b0d1eec940a039feb7ceecc11d1375b08
rodrigo <rfloresnu@unsa.edu.pe>
2025-07-23 18:45:29 -0500

    Add authentication guard

--- gameStore/frontend/src/router/index.js
+++ gameStore/frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/store/auth'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
