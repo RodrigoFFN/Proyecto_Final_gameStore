56938b166ac19c98ed793fb5329564a9d6cd2a9a
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 17:18:27 -0500

    Add rating attribute to review model.

--- gameStore/backend/ESGames/models/review.py
+++ gameStore/backend/ESGames/models/review.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from .user_profile import UserProfile
 from .videogame import Videogame
 
@@ -6,6 +7,7 @@ class Review(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     videogame = models.ForeignKey(Videogame, on_delete=models.CASCADE)
     comment = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
