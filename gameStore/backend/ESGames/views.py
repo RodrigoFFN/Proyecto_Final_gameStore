1924892174cef72e0fc3daf8454dc1e81741f117
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 17:28:34 -0500

    Add IsOwnerOrReadOnly view.

--- gameStore/backend/ESGames/views.py
+++ gameStore/backend/ESGames/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny, SAFE_METHODS, BasePermission
 
 from .models.cart import Cart, CartItem
 from .models.category import Category
@@ -18,6 +18,12 @@ from .serializers import (
     UserProfileSerializer, VideogameSerializer, RegisterSerializer
 )
 
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Lectura permitida para todos
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user.user == request.user
 
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
