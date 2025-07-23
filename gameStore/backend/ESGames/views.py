63bd1db43e9dfb9b6b455c9bd7b9951dcab539c3
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 17:31:25 -0500

    Add neccessary imports to views.py

--- gameStore/backend/ESGames/views.py
+++ gameStore/backend/ESGames/views.py
@@ -1,26 +1,29 @@
-from rest_framework import viewsets, generics
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.http import HttpResponse
+from io import BytesIO
+from datetime import datetime
+from rest_framework import viewsets, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny, SAFE_METHODS, BasePermission
-
+from rest_framework.decorators import api_view, permission_classes, action
 from .models.cart import Cart, CartItem
 from .models.category import Category
 from .models.library import Library
-from .models.rating import Rating
 from .models.review import Review
 from .models.user_profile import UserProfile
 from .models.videogame import Videogame
-
+from .models.favorite import Favorite
+from decimal import Decimal
 from .serializers import (
-    CartSerializer, CartItemSerializer,
-    CategorySerializer, LibrarySerializer,
-    RatingSerializer, ReviewSerializer,
+    CartSerializer, CartItemSerializer, FavoriteSerializer,
+    CategorySerializer, LibrarySerializer, ReviewSerializer,
     UserProfileSerializer, VideogameSerializer, RegisterSerializer
 )
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Lectura permitida para todos
         if request.method in SAFE_METHODS:
             return True
         return obj.user.user == request.user
