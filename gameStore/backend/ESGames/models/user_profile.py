cfaaf16aeded72bf92a5ab5d0093d4cb549abec5
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 17:19:11 -0500

    Add balance attribute to user profile model.

--- gameStore/backend/ESGames/models/user_profile.py
+++ gameStore/backend/ESGames/models/user_profile.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     address = models.TextField(blank=True)
     phone = models.CharField(max_length=9, blank=True)
     is_admin = models.BooleanField(default=False)
