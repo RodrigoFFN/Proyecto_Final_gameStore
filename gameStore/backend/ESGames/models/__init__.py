635f613869d4b8c90e2854a11dae381593db5678
Mauricio Antonio Paredes Miranda <mparedesmir@unsa.edu.pe>
2025-07-23 17:15:28 -0500

    Add favorite model import to __init__.

--- gameStore/backend/ESGames/models/__init__.py
+++ gameStore/backend/ESGames/models/__init__.py
@@ -3,5 +3,5 @@ from .category import Category
 from .videogame import Videogame
 from .cart import Cart, CartItem
 from .review import Review
-from .rating import Rating
 from .library import Library
+from .favorite import Favorite
