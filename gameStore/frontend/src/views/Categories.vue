56e0de157b8a242dc7cfb2192e41b45b15a18c04
Mijael Paul Leon Ramos <mleonra@unsa.edu.pe>
2025-07-23 22:38:03 -0500

    Add template to list all categories with links

---
+++ gameStore/frontend/src/views/Categories.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Categorías</h1>
+    <ul>
+      <li v-for="cat in categories" :key="cat.id">
+        <router-link :to="`/category/${cat.id}`">{{ cat.name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
