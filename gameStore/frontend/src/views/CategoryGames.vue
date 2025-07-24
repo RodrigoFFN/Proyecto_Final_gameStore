2ea771689cc5b7c18a24efb56deeebd20647022e
rodrigo <rfloresnu@unsa.edu.pe>
2025-07-23 22:04:25 -0500

    Display games by selected category

---
+++ gameStore/frontend/src/views/CategoryGames.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Videojuegos de categoría {{ categoryId }}</h2>
+    <div v-if="games.length">
+      <ul>
+        <li v-for="game in games" :key="game.id">
+          <router-link :to="`/game/${game.id}`">{{ game.title }}</router-link>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>No hay juegos en esta categoría.</p>
+    </div>
+  </div>
+</template>
+
